55b64a81821875dfabd7cad2e6b58061efbad350
Gaurav Jayant <gaurav.jayant@groupbayport.com>
2026-09-17 23:41:41 +0530

    Fixed

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
-a =2
+a =6
 b=3
-print("Addition of a and b " "2" "+" "3" " =",a+b)
+print("Addition of a and b " "6" "+" "3" " =",a+b)
+print("Subtraction of a and b " "6" "-" "3" " =",a-b)
+print("Multiply of a and b " "6" "*" "3" " =",a*b)
+print("Div of a and b " "6" "/" "3" " =",a/b)
